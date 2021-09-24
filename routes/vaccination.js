7548744737508fd50c077272afbd16f5cdb2da57
Gla <watchrapong.gla@mail.kmutt.ac.th>
2021-09-24 23:54:40 +0700

    [fix] vaccination.js

--- routes/vaccination.js
+++ routes/vaccination.js
@@ -10,152 +10,40 @@ router.get("/acc", async (req, res) => {
   res.json({ account: account });
 });
 
-router.post("/person/add1", async (req, res) => {
-  var form = new formidable.IncomingForm();
-  form.parse(req ,async (err, field) =>{
-    const {
-    citizenIdInput,
-    firstNameInput,
-    lastNameInput,
-    genderInput,
-    vaccineName1,
-    hospitalName1,
-  } = field;
-    if (
-      !citizenIdInput ||
-      !firstNameInput ||
-      !lastNameInput ||
-      !genderInput ||
-      !vaccineName1 ||
-      !hospitalName1
-    ) {
-      return res.json({
-        success: false,
-        error: "Please fill the form.",
-      });
-    }
-    var nowDate = new Date();
-    var dateGetVaccine1 =
-      nowDate.getFullYear() +
-      "/" +
-      (nowDate.getMonth() + 1) +
-      "/" +
-      nowDate.getDate();
+router.post("/person/addVaccine", async (req, res)=>{
+    var form = new formidable.IncomingForm();
+    form.parse(req ,async (err, field) =>{
+      console.log(field)
+      if(field.Dose === "1"){
+      const {
+      citizenIdInput,
+      firstNameInput,
+      lastNameInput,
+      genderInput,
+      vaccineName,
+      dateGetVaccine,
+      hospitalName
+    } = field;
     var vaccineName2 = "";
     var dateGetVaccine2 = "";
     var hospitalName2 = "";
     var registrarProvider = RegistrarWeb3Provider(privateKey);
     var account = await registrarProvider.web3.eth.getAccounts();
-    //var balance = await registrarProvider.web3.eth.getBalance(account[0]);
-    //var firstNoune = registrarProvider.web3.eth.getTransactionCount(account[0]);
-    //console.log("Account : ", account);
-    //console.log("Nounce : ", await firstNoune);
-    try {
-      console.log("1");
-      return await registrarProvider.transcript.methods
-        .addVaccine(
-          citizenIdInput,
-          firstNameInput,
-          lastNameInput,
-          genderInput,
-          vaccineName1,
-          dateGetVaccine1,
-          hospitalName1,
-          vaccineName2,
-          dateGetVaccine2,
-          hospitalName2
-        )
-        .send({ from: account[0] })
-        .then((receipt) => {
-          console.log("Result : ", receipt);
-          console.log("Receipt : ", receipt.status);
-          if (receipt.status) {
-            status = true;
-            //hash = transactionHash;
-          } else {
-            status = true;
-            //hash = transactionHash;
-          }
-          return { status: status };
-          // if (!err) {
-          //     status = true;
-          //     hash = transactionHash;
-  
-          // } else {
-          //     status = false;
-          //     hash = "No Transaction Hash"
-          // }
-          // //console.log("Status : "+status)
-        });
-    } catch (err) {
-      console.error("" + err);
-    }
-  })
-
-  
-});
-
-router.post("/person/add2", async (req, res) => {
-  const {
-    citizenIdInput,
-    firstNameInput,
-    lastNameInput,
-    genderInput,
-    vaccineName1,
-    hospitalName1,
-    dateGetVaccine1,
-    vaccineName2,
-    hospitalName2,
-  } = req.body;
-
-  if (
-    !citizenIdInput ||
-    !firstNameInput ||
-    !lastNameInput ||
-    !genderInput ||
-    !vaccineName1 ||
-    !hospitalName1 ||
-    !dateGetVaccine1 ||
-    !vaccineName2 ||
-    !hospitalName2
-  ) {
-    return res.json({
-      success: false,
-      error: "Please fill the form.",
-    });
-  }
-  var nowDate = new Date();
-  var dateGetVaccine2 =
-    nowDate.getFullYear() +
-    "/" +
-    (nowDate.getMonth() + 1) +
-    "/" +
-    nowDate.getDate();
-  var registrarProvider = RegistrarWeb3Provider(privateKey);
-  var account = await registrarProvider.web3.eth.getAccounts();
-  //var balance = await registrarProvider.web3.eth.getBalance(account[0]);
-  //var firstNoune = registrarProvider.web3.eth.getTransactionCount(account[0]);
-  //console.log("Account : ", account);
-  //console.log("Nounce : ", await firstNoune);
-  try {
-    console.log("1");
-    return await registrarProvider.transcript.methods
-      .addVaccine(
-        citizenIdInput,
-        firstNameInput,
-        lastNameInput,
-        genderInput,
-        vaccineName1,
-        dateGetVaccine1,
-        hospitalName1,
-        vaccineName2,
-        dateGetVaccine2,
-        hospitalName2
-      )
-      .send({ from: account[0] })
-      .then((receipt) => {
-        console.log("Result : ", receipt);
-        console.log("Receipt : ", receipt.status);
+    try{
+      console.log("start")
+    return await registrarProvider.transcript.methods.addVaccine(
+      citizenIdInput,
+      firstNameInput,
+      lastNameInput,
+      genderInput,
+      vaccineName,
+      dateGetVaccine,
+      hospitalName,
+      vaccineName2,
+      dateGetVaccine2,
+      hospitalName2).send({ from: account[0]}).then((receipt) => {
+        console.log("Receipt : ", receipt);
+        console.log("Status : ", receipt.status);
         if (receipt.status) {
           status = true;
           //hash = transactionHash;
@@ -163,42 +51,67 @@ router.post("/person/add2", async (req, res) => {
           status = true;
           //hash = transactionHash;
         }
+        res.json({result: true})
         return { status: status };
-        // if (!err) {
-        //     status = true;
-        //     hash = transactionHash;
-
-        // } else {
-        //     status = false;
-        //     hash = "No Transaction Hash"
-        // }
-        // //console.log("Status : "+status)
       });
-  } catch (err) {
-    console.error("" + err);
-  }
-});
-
-router.post("/person/:citizenid", async (req, res) => {
-  const { citizenid } = req.params;
-  var registrarProvider = RegistrarWeb3Provider(privateKey);
-  var addressData = await registrarProvider.transcript.methods
-    .getVaccine(citizenid)
-    .call((err, res) => {
-      if (!err) {
-        return res;
+    } catch (err) {
+      console.error("error" + err);
+      res.json({result: false});
+    }
+  }else{
+    const {
+      citizenIdInput,
+      firstNameInput,
+      lastNameInput,
+      genderInput,
+      vaccineName,
+      dateGetVaccine,
+      hospitalName
+    } = field;
+    var registrarProvider = RegistrarWeb3Provider(privateKey);
+    var addressData = await registrarProvider.transcript.methods.getVaccine(citizenIdInput).call();
+    var vaccineName1 = addressData.vaccineName1;
+    var dateGetVaccine1 = addressData.dateGetVaccine1;
+    var hospitalName1 = addressData.hospitalName1;
+    var registrarProvider = RegistrarWeb3Provider(privateKey);
+    var account = await registrarProvider.web3.eth.getAccounts();
+    try{
+      console.log("start");
+    return await registrarProvider.transcript.methods
+    .addVaccine(
+      citizenIdInput,
+      firstNameInput,
+      lastNameInput,
+      genderInput,
+      vaccineName1,
+      dateGetVaccine1,
+      hospitalName1,
+      vaccineName,
+      dateGetVaccine,
+      hospitalName,
+    ).send({ from: account[0]}).then((receipt) => {
+      console.log("Receipt : ", receipt);
+      console.log("Status : ", receipt.status);
+      if (receipt.status) {
+        status = true;
+        //hash = transactionHash;
       } else {
-        console.log(err);
+        status = true;
+        //hash = transactionHash;
       }
+      res.json({result: true});
+      return { status: status };
     });
-  console.log(addressData);
-  res.json({
-    result: addressData,
-  });
-});
+  } catch (err) {
+    console.error("error" + err);
+    res.json({result: false});
+  }
+  }
+})    
+ 
+})
 
 router.post("/vaccine/:citizenid", async (req, res) => {
-  //   try {
   const { citizenid } = req.params;
   var registrarProvider = RegistrarWeb3Provider(privateKey);
   var addressData = await registrarProvider.transcript.methods
@@ -211,23 +124,9 @@ router.post("/vaccine/:citizenid", async (req, res) => {
         console.log(err);
       }
     });
-
   res.json({
     result: addressData,
   });
-  //       const result = await getVaccine(citizenid);
-  //       return res.json({
-  //          success: true,
-  //          data: result,
-  //          error: null,
-  //       });
-  //   }
-  //   catch (error) {
-  //     return res.json({
-  //       success: false,
-  //       error: error.message,
-  //     });
-  //   }
 });
 
 module.exports = router;
