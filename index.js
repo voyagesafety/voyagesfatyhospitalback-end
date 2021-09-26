46976678ca871b06498cc1615639945675e437fe
Gla <watchrapong.gla@mail.kmutt.ac.th>
2021-09-26 17:15:57 +0700

    [edit] env port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require("cors");
 const app = express();
-const appPort = process.env.PORT || 4000;
+const appPort = process.env.PORT;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
