9696136285ed9a9ea9214db0ef0cf374d88f8f62
Gla <watchrapong.gla@mail.kmutt.ac.th>
2021-09-26 17:35:09 +0700

    [edit] package.json

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require("cors");
 const app = express();
-const appPort = process.env.PORT;
+const appPort = process.env.PORT || 4000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
