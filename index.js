c39da7827cb7496877bafb296a2adc2924943d57
Gla <watchrapong.gla@mail.kmutt.ac.th>
2021-11-30 00:20:12 +0700

    [edit] cors

--- index.js
+++ index.js
@@ -7,10 +7,25 @@ const appPort = process.env.PORT || 4000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-const corsOptions = {
-   origin: ['https://voyagesafetybackend.ninja','https://voyage-safety-frontend.herokuapp.com']
-}
-app.use(cors(corsOptions));
+// const corsOptions = {
+//    origin: ['https://voyagesafetybackend.ninja','https://voyage-safety-frontend.herokuapp.com']
+// }
+// app.use(cors(corsOptions));
+
+app.use(function (req, res, next) {
+
+   var allowedDomains = ['https://voyagesafetybackend.ninja','https://voyage-safety-frontend.herokuapp.com'];
+   var origin = req.headers.origin;
+   if(allowedDomains.indexOf(origin) > -1){
+     res.setHeader('Access-Control-Allow-Origin', origin);
+   }
+ 
+   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Accept');
+   res.setHeader('Access-Control-Allow-Credentials', true);
+ 
+   next();
+ })
 
 app.get('/', (req, res, next) => {
    res.send('Hello! Welcome to Application!');
