d756492caec08aee2adb68adcbfd9861173fe4f1
Gla <watchrapong.gla@mail.kmutt.ac.th>
2021-12-07 12:44:11 +0700

    [edit] constant

--- Connection.js
+++ Connection.js
@@ -10,7 +10,7 @@ const HRWeb3Provider = () => {
     const mnemonic = centralMnemonic;
     const provider = new HDWalletProvider(mnemonic, infuraNode);
     const web3 = new Web3(provider);
-    var vaccinationAddress = "0x1B20e47E405634b9f04E2FE9E5621fFEA177f2dc";
+    var vaccinationAddress = constants.contractAddress;
     var vaccination = new web3.eth.Contract(ABI.abi, vaccinationAddress);
     return {web3:web3,vaccination:vaccination};
 }
@@ -19,7 +19,7 @@ const RegistrarWeb3Provider = (mnemonic) => {
     
     const provider = new HDWalletProvider(mnemonic, infuraNode);
     const web3 = new Web3(provider);
-    var vaccinationAddress = "0x1B20e47E405634b9f04E2FE9E5621fFEA177f2dc";
+    var vaccinationAddress = constants.contractAddress;
     var vaccination = new web3.eth.Contract(ABI.abi, vaccinationAddress);
     return {web3:web3,vaccination:vaccination};
 }
--- constant.js
+++ constant.js
@@ -5,4 +5,5 @@ module.exports =
     mnemonic : 'verb dentist ranch lounge virtual code scrap fruit risk exchange fame divide',
     infuraNode: `wss://rinkeby.infura.io/ws/v3/ae0c4db814424f40a171abaad255481c`,
     privateKey: '0015de89ee814871af24848a812f3c20d694d969c7919d36ce07f7126eb02c88',
+    contractAddress: "0x1B20e47E405634b9f04E2FE9E5621fFEA177f2dc",
 }
