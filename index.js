797f05606b11b76cb0bb450c5653e9f346912427
Gla <watchrapong.gla@mail.kmutt.ac.th>
2021-12-08 17:25:03 +0700

    [edit] cors

--- index.js
+++ index.js
@@ -7,26 +7,26 @@ const appPort = process.env.PORT || 4000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// const corsOptions = {
-//    origin: ['https://voyagesafetybackend.ninja','https://voyage-safety-frontend.herokuapp.com']
-// }
-// app.use(cors(corsOptions));
-app.use(cors);
+const corsOptions = {
+   origin: ['https://voyagesafetybackend.ninja','https://voyage-safety-frontend.herokuapp.com','https://voyage-hospital-frontend.herokuapp.com'];
+}
+app.use(cors(corsOptions));
+// app.use(cors);
 
-app.use(function (req, res, next) {
+// app.use(function (req, res, next) {
 
-   var allowedDomains = ['https://voyagesafetybackend.ninja','https://voyage-safety-frontend.herokuapp.com','https://voyage-hospital-frontend.herokuapp.com'];
-   var origin = req.headers.origin;
-   if(allowedDomains.indexOf(origin) > -1){
-     res.setHeader('Access-Control-Allow-Origin', origin);
-   }
+//    var allowedDomains = ['https://voyagesafetybackend.ninja','https://voyage-safety-frontend.herokuapp.com','https://voyage-hospital-frontend.herokuapp.com'];
+//    var origin = req.headers.origin;
+//    if(allowedDomains.indexOf(origin) > -1){
+//      res.setHeader('Access-Control-Allow-Origin', origin);
+//    }
  
-   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Accept');
-   res.setHeader('Access-Control-Allow-Credentials', true);
+//    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+//    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Accept');
+//    res.setHeader('Access-Control-Allow-Credentials', true);
  
-   next();
- })
+//    next();
+//  })
 
 app.get('/', (req, res, next) => {
    res.send('Hello! Welcome to Application!');
